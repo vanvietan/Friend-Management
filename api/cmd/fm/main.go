ae599a9addcc45251dcbe1836b8683b67300c8c6
vanvietan <vanvietan2911@gmail.com>
2022-10-31 14:38:43 +0700

    feat: database connection

--- api/cmd/fm/main.go
+++ api/cmd/fm/main.go
@@ -1,1 +1,15 @@
-package fm
+package main
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+func main() {
+	port := ":8000"
+	r := chi.NewRouter()
+
+	fmt.Println("Serving on " + port)
+	http.ListenAndServe(port, r)
+}
