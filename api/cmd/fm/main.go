60b93a7cc42c8c976cc2100b25cbf14b51fa5e37
vanvietan <vanvietan2911@gmail.com>
2022-11-01 14:01:57 +0700

    feat: setting up router

--- api/cmd/fm/main.go
+++ api/cmd/fm/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fm/api/internal/api/router"
 	"fm/api/internal/config"
+	relationshipSvc "fm/api/internal/controller/relationship"
+	userSvc "fm/api/internal/controller/user"
+	relationshipRepo "fm/api/internal/repository/relationship"
+	userRepo "fm/api/internal/repository/user"
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	log "github.com/sirupsen/logrus"
@@ -13,10 +18,16 @@ func main() {
 	r := chi.NewRouter()
 
 	//Create database connection
-	_, err := config.GetDatabaseConnection()
+	dbConn, err := config.GetDatabaseConnection()
 	if err != nil {
 		log.Fatal("encountered error when create a db connection, error :%v", err)
 	}
+
+	userRepository := userRepo.New(dbConn)
+	relationshipRepository := relationshipRepo.New(dbConn)
+	userService := userSvc.New(userRepository)
+	relationshipService := relationshipSvc.New(relationshipRepository)
+	router.New(r, userService, relationshipService)
 
 	fmt.Println("Serving on " + port)
 	http.ListenAndServe(port, r)
